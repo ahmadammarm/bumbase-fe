eeec96c8e985a02d5f0ccbbe8d7d774aba75b676
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-07-16 09:16:28 +0700

    feat(auth): add register page

---
+++ src/views/Register.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useRegister } from "@/hooks/auth/useRegister";
+import { ErrorValidations } from "@/types/ErrorValidation";
+import { useState, type FC, type FormEvent } from "react";
+import { useNavigate } from "react-router";
+
+const Register: FC = () => {
+    const navigate = useNavigate();
+
+    const { mutate, isPending } = useRegister();
+
+    const [name, setName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const [errors, setErrors] = useState<ErrorValidations>({});
+
+    const handleRegister = async (e: FormEvent) => {
+        e.preventDefault();
+
+        mutate({
+            name,
+            email,
+            password
+        }, {
+            onSuccess: () => {
+                navigate('/login')
+            },
+            onError: (error: any) => {
+                setErrors(error.response.data.errors)
+            }
+        })
+    }
+
+}
